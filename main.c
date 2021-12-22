53b2ff85352d47faf2380f121d2454a22269f2b5
tordpum <54734592+tordpum@users.noreply.github.com>
2021-12-23 00:26:49 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long long int main()
+{
+    int i,a[300];
+    long long int m,n,sum;
+    scanf("%lld",&n);
+    scanf("%lld",&m);
+    printf("%lld",n+m);
+    return 0;
+}
